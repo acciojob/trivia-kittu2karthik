259fa2d2df262e72ae1dd1ca55c762fb6b70999b
kittu2karthik <71444538+kittu2karthik@users.noreply.github.com>
2025-10-21 15:46:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-//your code here
+// //your code here
 
-const radio = document.getElementById('new_Delhi');
+// const radio = document.getElementById('new_Delhi');
 
-radio.addEventListener('change', () => {
-            if (radio.checked) {
-                const html = `
-                        <blockquote class="quote" cite="https://en.wikipedia.org/wiki/New_Delhi">New Delhi is the capital of India and an administrative district of NCT Delhi.</blockquote>
-                `;
-                document.body.insertAdjacentHTML('beforeend', html);
-            }
-        });
+// radio.addEventListener('change', () => {
+//             if (radio.checked) {
+//                 const html = `
+                       
+//                 `;
+//                 document.body.insertAdjacentHTML('beforeend', html);
+//             }
+//         });
